8246fede68ba2749407ea06bc2c337d6c2939247
Iris Xu <irisxu02@gmail.com>
2022-06-09 21:50:20 -0700

    Get parameters from form

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,15 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String name = request.getParameter("name-input");
+    String email = request.getParameter("email-input");
+    String subject = request.getParameter("subject-input");
+    String message = request.getParameter("message-input");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("You submitted: " + name);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("You submitted: " + name);
   }
 }
